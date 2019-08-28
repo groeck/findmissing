49ba1c8f5f105091494b10d8b47a1335d1675bdf
Guenter Roeck <groeck@chromium.org>
2019-08-28 15:01:00 -0700

    Add setup for chromeos repository
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-"
 
 kernel_site = "https://git.kernel.org/"
-
 upstream_repo = kernel_site + "pub/scm/linux/kernel/git/torvalds/linux"
 stable_repo = kernel_site + "pub/scm/linux/kernel/git/stable/linux-stable"
 
-stable_branches = ('4.4', '4.9', '4.14', '4.19', '5.2')
+chromium_site = "https://chromium.googlesource.com/"
+chromeos_repo = chromium_site + "chromiumos/third_party/kernel"
 
+stable_branches = ('4.4', '4.9', '4.14', '4.19', '5.2')
+stable_pattern = 'linux-%s.y'
+
+chromeos_branches = ('4.4', '4.14', '4.19')
+chromeos_pattern = 'chromeos-%s'
+
+chromeos_path = "linux-chrome"
 stable_path = "linux-stable"
 upstream_path = "linux-upstream"
--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@
 stable_path=$(python -c "from config import stable_path; print stable_path;")
 stable_repo=$(python -c  "from config import stable_repo; print stable_repo;")
 
+chromeos_path=$(python -c "from config import chromeos_path; print chromeos_path;")
+chromeos_repo=$(python -c  "from config import chromeos_repo; print chromeos_repo;")
+
 upstream_path=$(python -c "from config import upstream_path; print upstream_path;")
 if [[ "$(dirname ${upstream_path})" = "." ]]; then
 	# Needs to be an absolute path name
@@ -13,7 +16,10 @@ if [[ "$(dirname ${upstream_path})" = "." ]]; then
 fi
 upstream_repo=$(python -c  "from config import upstream_repo; print upstream_repo;")
 
-branches=($(python -c "from config import stable_branches; print stable_branches" | tr -d "(),'"))
+sbranches=($(python -c "from config import stable_branches; print stable_branches" | tr -d "(),'"))
+spattern=($(python -c "from config import stable_pattern; print stable_pattern" | tr -d "(),'"))
+cbranches=($(python -c "from config import chromeos_branches; print chromeos_branches" | tr -d "(),'"))
+cpattern=($(python -c "from config import chromeos_pattern; print chromeos_pattern" | tr -d "(),'"))
 
 # Simple clone:
 # Clone repository, do not add 'upstream' remote
@@ -44,13 +50,14 @@ clone_simple()
 clone_simple "${upstream_path}" "${upstream_repo}"
 
 # Complex clone:
-# Clone repository, add 'upstream' and 'next' remotes,
+# Clone repository, add 'upstream' remote,
 # check out and update list of branches
 clone_complex()
 {
     local destdir=$1
     local repository=$2
-    local branches=("${!3}")
+    local pattern=$3
+    local branches=("${!4}")
 
     echo "Cloning ${repository} into ${destdir}"
 
@@ -59,7 +66,7 @@ clone_complex()
 	git reset --hard HEAD
 	git fetch origin
 	for branch in ${branches[*]}; do
-	    branch="linux-${branch}.y"
+	    branch="$(printf ${pattern} ${branch})"
 	    if git rev-parse --verify "${branch}" >/dev/null 2>&1; then
 		git checkout "${branch}"
 		if ! git pull; then
@@ -80,17 +87,17 @@ clone_complex()
 	git clone "${repository}" "${destdir}"
 	pushd "${destdir}" >/dev/null
 	for branch in ${branches[*]}; do
+	    branch="$(printf ${pattern} ${branch})"
 	    git checkout -b "${branch}" "origin/${branch}"
 	done
 	git remote add upstream "${upstream_path}"
 	git fetch upstream
-	git remote add next "${next_path}"
-	git fetch next
 	popd >/dev/null
     fi
 }
 
-clone_complex "${stable_path}" "${stable_repo}" branches[*]
+clone_complex "${stable_path}" "${stable_repo}" "${spattern}" sbranches[*]
+clone_complex "${chromeos_path}" "${chromeos_repo}" "${cpattern}" cbranches[*]
 
 echo "Initializing databases"
 python initdb.py
