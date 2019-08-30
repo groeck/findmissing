a085c399a8972fcccb39fbe825a5658c36ca829b
Guenter Roeck <groeck@chromium.org>
2019-08-30 14:17:18 -0700

    config.py: Add 3.18 / chromeos-3.18
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ stable_repo = kernel_site + "pub/scm/linux/kernel/git/stable/linux-stable"
 chromium_site = "https://chromium.googlesource.com/"
 chromeos_repo = chromium_site + "chromiumos/third_party/kernel"
 
-stable_branches = ('4.4', '4.9', '4.14', '4.19', '5.2')
+stable_branches = ('3.18', '4.4', '4.9', '4.14', '4.19', '5.2')
 stable_pattern = 'linux-%s.y'
 
-chromeos_branches = ('4.4', '4.14', '4.19')
+chromeos_branches = ('3.18', '4.4', '4.14', '4.19')
 chromeos_pattern = 'chromeos-%s'
 
 chromeos_path = "linux-chrome"
